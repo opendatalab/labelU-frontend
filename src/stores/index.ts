38a16e205012659692da02177b717a7b3d07ab45
michale <1678728427@qq.com>
2022-12-19 14:09:26 +0800

    fix v3

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -5,7 +5,7 @@ import rootReducer from './rootReducer';
 const store = configureStore({
   reducer: rootReducer
 });
-
+//
 export type AppState = ReturnType<typeof rootReducer>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
