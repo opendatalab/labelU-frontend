fc9d42d990354c81af142f45b9836d28c8137aab
PJLAB\huangpeng <huangpeng@pjlab.org.cn>
2023-01-10 11:38:27 +0800

    fix typography v1

--- src/locales/zh-CN/LoginAndUp/index.ts
+++ src/locales/zh-CN/LoginAndUp/index.ts
@@ -1,7 +1,7 @@
 export const LoginAndUp = {
   'loginTitle1' : '一款统一、通用、灵活、超出想象的',
   'loginTitle2' : '标注工具',
-  'login123' : '登陆',
+  'login123' : '登录',
   'email' : '邮箱',
   'password' : '密码',
   'signUp' : '注册',
