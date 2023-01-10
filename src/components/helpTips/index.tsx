61560af706bdfc8b19fd2835308a9e3da643a74b
jinzhenj <jinzhenjiang@pjlab.org.cn>
2023-01-10 14:36:41 +0800

    feat: 帮助文档需要链接到帮助文档

--- src/components/helpTips/index.tsx
+++ src/components/helpTips/index.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { BellOutlined } from '@ant-design/icons';
-import { Modal } from 'antd';
 import currentStyles from './index.module.scss';
-const HelpTips = (props : any)=>{
-    const clickShowModal = ()=>{
-        Modal.success({
-            title : '标注文档',
-            content : props.taskTips
-        })
-    }
 
-    return (<div onClick = {()=>clickShowModal()} className={currentStyles.outerFrame}>帮助文档&nbsp;&nbsp;<img src = '/src/icons/helpText.svg' /></div>)
+const HelpLink = "https://github.com/opendatalab/labelU/blob/main/docs/GUIDE.md";
+const LinkColor = "rgba(0, 0, 0, 0.85)"
+
+const HelpTips = (props : any)=>{
+    return (<div className={currentStyles.outerFrame}>
+        <a href={HelpLink} target="_blank" style={{"color": LinkColor}}>
+            帮助文档&nbsp;&nbsp;<img src = '/src/icons/helpText.svg' />
+        </a>
+    </div>);
 }
+
 export default HelpTips;
